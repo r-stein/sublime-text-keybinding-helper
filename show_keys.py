0a3175737c4123137b5e76cb6a683a960bfdfc5c
Richard Stein <r-stein.github@web.de>
2016-03-18 00:33:41 +0100

    Added a command to store the current "show_input" state
    This command stores the state Application instead of window wide and offers ST2 compatibility

--- show_keys.py
+++ show_keys.py
@@ -2,10 +2,18 @@ import sublime
 import sublime_plugin
 
 
+def toggle_show_input(name):
+    """Returns and toggles the current show state"""
+    state = toggle_show_input.states.get(name, True)
+    toggle_show_input.states[name] = not state
+    return state
+
+toggle_show_input.states = {}
+
+
 class ToggleShowCommandsCommand(sublime_plugin.WindowCommand):
     def run(self):
-        show_commands = not self.window.settings().get("kh_do_show_commands")
-        self.window.settings().set("kh_do_show_commands", show_commands)
+        show_commands = toggle_show_input("show_commands")
         sublime.log_commands(show_commands)
         print("Show commands: {0}".format(show_commands))
         if show_commands:
@@ -14,8 +22,7 @@ class ToggleShowCommandsCommand(sublime_plugin.WindowCommand):
 
 class ToggleShowInputsCommand(sublime_plugin.WindowCommand):
     def run(self):
-        show_input = not self.window.settings().get("kh_do_show_input")
-        self.window.settings().set("kh_do_show_input", show_input)
+        show_input = toggle_show_input("show_input")
         sublime.log_input(show_input)
         print("Show input: {0}".format(show_input))
         if show_input:
